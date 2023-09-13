20f5bceeee6b2bd1a8cd4b1602880d1dc55850c6
alexo <alexo@Debian.myguest.virtualbox.org>
2023-09-12 21:11:38 -0600

    Iniciando Clase del 12 sep AreaBean, FrmArea lleva errores en Frm

---
+++ src/main/java/sv/edu/occ/ues/ingenieria/prn335/parqueowebapp/boundary/jsf/FrmArea.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sv.edu.occ.ues.ingenieria.prn335.parqueowebapp.boundary.jsf;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
+import sv.edu.occ.ues.ingenieria.prn335.parqueowebapp.app.entity.Area;
+import sv.edu.occ.ues.ingenieria.prn335.parqueowebapp.control.AbstractDataAccess;
+import sv.edu.occ.ues.ingenieria.prn335.parqueowebapp.control.AreaBean;
+
+/**
+ *
+ * @author alexo
+ */
+public class FrmArea extends AbstractFrm<Area> implements Serializable {
+
+    @Inject
+    FacesContext fc;
+    @Inject
+    AreaBean aBean;
+    TreeNode raiz;
+    TreeNode nodoSeleccionado;
+
+    @PostConstruct
+    @Override
+    public void inicializar() {
+        super.inicializar();
+        this.raiz = new DefaultTreeNode("Areas", null);
+        List<Area> lista = aBean.findByIdPadre(null, 0, 100000000);
+
+        if (lista != null && !lista.isEmpty()) {
+
+            for (Area next : lista) {
+                if (next.getIdAreaPadre() == null) {
+                    this.generarArbol();
+                }
+
+            }
+        }
+    }
+    
+    
+    public void generarArbol(TreeNode padre, Area actual){
+        DefaultTreeNode nuevoPadre =new Defa
+    
+    }
+
+    @Override
+    public AbstractDataAccess<Area> getDataAccess() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public FacesContext getFacesContext() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getIdPorObjeto(Area object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Area getObjetoPorId(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void instanciarRegistro() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
---
+++ src/main/java/sv/edu/occ/ues/ingenieria/prn335/parqueowebapp/control/AreaBean.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sv.edu.occ.ues.ingenieria.prn335.parqueowebapp.control;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+import sv.edu.occ.ues.ingenieria.prn335.parqueowebapp.app.entity.Area;
+import sv.edu.occ.ues.ingenieria.prn335.parqueowebapp.app.entity.TipoReserva;
+
+/**
+ *
+ * @author alexo
+ */
+public class AreaBean extends AbstractDataAccess<Area> implements Serializable{
+    
+
+   @PersistenceContext(unitName = "parqueoPU")
+    EntityManager em;
+
+    @Override
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AreaBean() {
+        super(Area.class);
+    }
+    
+    public List<Area> findByIdPadre(Area idPadre, int first, int pageSize) {
+        if (idPadre != null && em != null) {
+            Query q = em.createNamedQuery("Area.findByIdPadre");
+            q.setFirstResult(first);
+            q.setMaxResults(pageSize);
+            q.setParameter("idPersona", idPadre);
+            return q.getResultList();
+        }
+        return Collections.EMPTY_LIST;
+    }
+    
+}
